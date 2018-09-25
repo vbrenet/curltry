e39f017902d7d9b6cdcd77577a4fb32fc76de9d1
Vincent Brenet <vbrenet@gmail.com>
2018-09-25 15:12:44 +0200

    Initial Commit

---
+++ curltry/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  curltry
+//
+//  Created by Vincent Brenet on 25/09/2018.
+//  Copyright © 2018 Vincent Brenet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
