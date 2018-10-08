bb3e3df0b41f36cd50ed1b933fee6fe3083d3d20
Vincent Brenet <vbrenet@gmail.com>
2018-10-08 14:01:17 +0200

    bulk session creation : in progress

---
+++ curltry/bulkSession.cpp
@@ -0,0 +1,15 @@
+//
+//  bulkSession.cpp
+//  curltry
+//
+//  Created by Vincent Brenet on 08/10/2018.
+//  Copyright © 2018 Vincent Brenet. All rights reserved.
+//
+
+#include "bulkSession.hpp"
+
+bool bulkSession::openBulkSession(bool isSandbox, const std::string username, const std::string password) {
+    bool result {false};
+    
+    return result;
+}
---
+++ curltry/bulkSession.hpp
@@ -0,0 +1,21 @@
+//
+//  bulkSession.hpp
+//  curltry
+//
+//  Created by Vincent Brenet on 08/10/2018.
+//  Copyright © 2018 Vincent Brenet. All rights reserved.
+//
+
+#ifndef bulkSession_hpp
+#define bulkSession_hpp
+
+#include <stdio.h>
+#include <string>
+
+#endif /* bulkSession_hpp */
+
+class bulkSession {
+private:
+public:
+    static bool openBulkSession(bool isSandbox, const std::string username, const std::string password);
+};
