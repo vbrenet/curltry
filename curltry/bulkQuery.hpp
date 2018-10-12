4ee6cd16d2b2f3b5506f6739c9a2731c2605a405
Vincent Brenet <vbrenet@gmail.com>
2018-10-12 17:54:48 +0200

    commented bulkQuery header file

--- curltry/bulkQuery.hpp
+++ curltry/bulkQuery.hpp
@@ -12,7 +12,9 @@
 #include <stdio.h>
 #include <string>
 #include <map>
-
+//
+//  job info provided by Salesforce during job execution
+//
 struct jobStatusInfo {
     std::string status;
     std::string concurrencyMode;
@@ -27,38 +29,40 @@ struct jobStatusInfo {
     int totalProcessingTime;
     int apiActiveProcessingTime;
     int apexProcessingTime;
-    void print() const;
+    void print() const;     // print helper method (standard output)
 };
+//
 //
 //
 struct batchInfo {
-    std::string status;
-    bool isRead;
+    std::string status; // info provided by Salesforce (e.g. "completed")
+    bool isRead;        // true if results (read data) have already been provided by bulkQuery class
     batchInfo(std::string s, bool b) : status{s}, isRead{b} {}
 };
 //
+//  class bulkQuery: encapsulate bulk API calls for queries, both in PKChunking mode or not
 //
 class bulkQuery {
 private:
-    static bool firstTime;
-    static std::string body;
-    static std::string jobId;
-    static std::string mainBatchId;
-    static jobStatusInfo jobInfo;
-    static std::map<std::string,batchInfo> batches; // id, infos
-    static bool pkchunking;
+    static bool firstTime;      // used by the callback providing data to libcurl
+    static std::string body;    // buffer containing POST request bodies
+    static std::string jobId;   // jobID of the bulkQuery session
+    static std::string mainBatchId; // main batch id of the bulkQuery session, never completed in case of PK chunking
+    static jobStatusInfo jobInfo;   // job info provided by the bulk API, related to the session's job
+    static std::map<std::string,batchInfo> batches; // map of ids and batch info of each batch related to the job
+    static bool pkchunking;     // true if we are in PK chunking mode
     
-    static size_t read_callback(void *dest, size_t size, size_t nmemb, void *userp);
-    static bool getJobStatus();
-    static void extractJobStatusInfo (const std::string&, jobStatusInfo&);
-    static bool getBatchesInfo();
-    static void extractBatchesInfo(const std::string&);
+    static size_t read_callback(void *dest, size_t size, size_t nmemb, void *userp); // callback called by libcurl to send data in POST requests
+    static bool getJobStatus(); // get job status, using bulk API resource
+    static void extractJobStatusInfo (const std::string&, jobStatusInfo&);  // helper method to extract job info
+    static bool getBatchesInfo();   // populate the batches map by all batch ids and status, using bulk API resource
+    static void extractBatchesInfo(const std::string&); // helper method to extract batch info
 
 public:
-    static bool createJob(const std::string objectName, int chunksize);
-    static bool addQuery(const std::string& query);
-    static bool waitCompletion();
-    static bool getResult(std::string& result);
+    static bool createJob(const std::string objectName, int chunksize); // first step to use the bulk API
+    static bool addQuery(const std::string& query); // second step : create a query batch
+    static bool waitCompletion();                   // third step : wait for completion of batches
+    static bool getResult(std::string& result);     // fourth step : get data result
 };
 
 #endif /* bulkQuery_hpp */
