9c2af700008863ba5a28ac82a8137e81e5632ec2
Vincent Brenet <vbrenet@gmail.com>
2018-11-16 10:06:31 +0100

    injectionOrchestrator class creation

---
+++ curltry/injectionOrchestrator.cpp
@@ -0,0 +1,9 @@
+//
+//  injectionOrchestrator.cpp
+//  curltry
+//
+//  Created by Vincent Brenet on 16/11/2018.
+//  Copyright © 2018 Vincent Brenet. All rights reserved.
+//
+
+#include "injectionOrchestrator.hpp"
---
+++ curltry/injectionOrchestrator.hpp
@@ -0,0 +1,28 @@
+//
+//  injectionOrchestrator.hpp
+//  curltry
+//
+//  Created by Vincent Brenet on 16/11/2018.
+//  Copyright © 2018 Vincent Brenet. All rights reserved.
+//
+
+#ifndef injectionOrchestrator_hpp
+#define injectionOrchestrator_hpp
+
+#include <stdio.h>
+#include <string>
+#include "sObject.hpp"
+#include "sessionCredentials.hpp"
+
+class injectionOrchestrator {
+private:
+    sObject theObject ;
+    sessionCredentials credentials;
+
+public:
+    injectionOrchestrator(const std::string sObjectName, const sessionCredentials& c): theObject {sObjectName}, credentials{c} {}
+
+};
+
+
+#endif /* injectionOrchestrator_hpp */
