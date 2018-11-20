f5b43c2f34f362d1b5361f6fd79447c2269dfda3
Vincent Brenet <vbrenet@gmail.com>
2018-11-20 11:24:25 +0100

    stable version

--- curltry/injectionOrchestrator.cpp
+++ curltry/injectionOrchestrator.cpp
@@ -30,7 +30,7 @@ bool injectionOrchestrator::execute() {
     std::string content = recgen.getCsvHeader();
     content += lf;
 
-    for (auto i=0; i != 1000; i++) {
+    for (auto i=0; i != 10000; i++) {
         content += recgen.getCsvRecord();
         content += lf;
     }
