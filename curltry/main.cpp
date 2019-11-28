a9229b0cdee889b68e93dcc611b01c9bde527466
Vincent Brenet <vbrenet@gmail.com>
2019-11-28 12:47:52 +0100

    version number

--- curltry/main.cpp
+++ curltry/main.cpp
@@ -29,7 +29,7 @@
 #include "globals.hpp"
 #include "buckets.hpp"
 
-const std::string curltryVersion = "curltry v2.1.0";
+const std::string curltryVersion = "curltry v2.1.1";
 //
 //
 void runGetResultFromId(const std::string& theObj,  const std::string& theId) {
