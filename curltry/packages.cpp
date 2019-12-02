55743cc63dc4c1ace0674a8dc0122c892db0e1fb
Vincent Brenet <vbrenet@gmail.com>
2019-12-02 17:51:43 +0100

    added packages - in progress

---
+++ curltry/packages.cpp
@@ -0,0 +1,41 @@
+//
+//  packages.cpp
+//  curltry
+//
+//  Created by Vincent Brenet on 02/12/2019.
+//  Copyright © 2019 Vincent Brenet. All rights reserved.
+//
+#include <iostream>
+#include <fstream>
+
+#include "packages.hpp"
+
+std::map <std::string, std::string> packages::packageMap;
+//
+//
+void packages::processLine (const std::string &inputline) {
+    
+    size_t semicolon = inputline.find_first_of(':');
+    
+    if (semicolon != std::string::npos) {
+        std::string key = inputline.substr(0,semicolon);
+        std::string value = inputline.substr(semicolon+1);
+        packageMap[key] = value;
+    }
+}
+//
+//
+void packages::initPackagesFromFile (const std::string &inputfile) {
+    std::ifstream packagesFile {inputfile};
+    
+    std::string currentLine;
+    
+    while (getline(packagesFile,currentLine)) {
+        // skip comments
+        if (currentLine[0] == '#')
+            continue;
+        processLine(currentLine);
+    }
+
+    packagesFile.close();
+}
---
+++ curltry/packages.hpp
@@ -0,0 +1,28 @@
+//
+//  packages.hpp
+//  curltry
+//
+//  Created by Vincent Brenet on 02/12/2019.
+//  Copyright © 2019 Vincent Brenet. All rights reserved.
+//
+
+#ifndef packages_hpp
+#define packages_hpp
+
+#include <stdio.h>
+#include <string>
+#include <map>
+
+class packages {
+private:
+    // map of raw value (e.g. FinServ) and commercial value
+    static std::map <std::string, std::string> packageMap;
+
+    static void processLine (const std::string &inputline);
+
+public:
+    static void initPackagesFromFile (const std::string &inputfile);
+    
+};
+
+#endif /* packages_hpp */
